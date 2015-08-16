beff6fc6901abec94d88d6f55ade10a953b00cf1
Adrian Jagusch <adrian.jagusch@we-mail.de>
2015-08-16 15:39:19 +0200

    Engine auf PysicsJS geändert, Info-Boxen

--- controllers/challenge.js
+++ controllers/challenge.js
@@ -1,13 +1,109 @@
 klimaChallenge.controller('ChallengeCtrl', function($scope, $http) {
 
+   Physics(function (world) {
+    // bounds of the window
+    var viewportBounds = Physics.aabb(0, 0, window.innerWidth, window.innerHeight)
+        ,edgeBounce
+        ,renderer
+        ;
+
+    // create a renderer
+    renderer = Physics.renderer('canvas', {
+        el: 'physics-world'
+    });
+
+    // add the renderer
+    world.add(renderer);
+    // render on each step
+    world.on('step', function () {
+        world.render();
+    });
+
+    // constrain objects to these bounds
+    edgeBounce = Physics.behavior('edge-collision-detection', {
+        aabb: viewportBounds,
+        restitution: 0.4
+    });
+
+    // initial resize (canvas expands to the bottom of the page)
+    viewportBounds = Physics.aabb(0, 0, renderer.width, renderer.height);
+    edgeBounce.setAABB(viewportBounds);
+
+    // resize events
+    window.addEventListener('resize', function () {
+
+        // as of 0.7.0 the renderer will auto resize... so we just take the values from the renderer
+        viewportBounds = Physics.aabb(0, 0, renderer.width, renderer.height);
+        // update the boundaries
+        edgeBounce.setAABB(viewportBounds);
+
+    }, true);
+
+    // create some bodies
+   var numberProjects = 25;
+   for (var i = 0; i < numberProjects; i++) {
+      var radius = Math.sqrt(renderer.width*1000/20/numberProjects)+15;
+      var project = Physics.body('circle', {
+        x: renderer.width * 0.4,
+        y: renderer.height * 0,
+        vx: 0.3,
+        radius: radius,
+        styles: {
+            fillStyle: '#59c134'
+        }
+      });
+      world.add(project);
+   }
+
+
+    world.on({
+        'interact:poke': function( pos ){
+            world.wakeUpAll();
+        }
+        ,'interact:move': function( pos ){
+        }
+        ,'interact:release': function(){
+            world.wakeUpAll();
+        }
+    });
+
+    // add things to the world
+    world.add([
+       Physics.behavior('interactive', { el: renderer.container }),
+       Physics.behavior('constant-acceleration', {acc: { x : 0, y: 0.0005 }}),
+       Physics.behavior('body-impulse-response'),
+       Physics.behavior('body-collision-detection'),
+       Physics.behavior('sweep-prune'),
+       edgeBounce
+    ]);
+
+    // subscribe to ticker to advance the simulation
+    Physics.util.ticker.on(function( time ) {
+        world.step( time );
+    });
+});
+
+
+/*
    // Matter.js module aliases
    var Engine = Matter.Engine,
        World = Matter.World,
-       Bodies = Matter.Bodies;
-       Body = Matter.Body;
+       Bodies = Matter.Bodies,
+       Body = Matter.Body,
+       Mouse = Matter.Mouse;
 
    var canvasWidth = document.getElementById('physics-world').offsetWidth;
-   var canvasHeight = document.getElementById('physics-world').offsetHeight;
+   var canvasHeight = document.getElementById('physics-world').offsetHeight-200;
+
+
+   var mouse = Matter.Mouse.create(
+     document.getElementById('physics-world').children[0],
+     {
+       enabledEvents: {
+         mousewheel: false
+       }
+     }
+   );
 
    // create a Matter.js engine
    var engine = Engine.create({
@@ -33,7 +129,11 @@ klimaChallenge.controller('ChallengeCtrl', function($scope, $http) {
 
    // Create balls
    var bodies = Array();
+<<<<<<< Updated upstream
    var numberProjects = 30;
+=======
+   var numberProjects = 25;
+>>>>>>> Stashed changes
    for (var i = 0; i < numberProjects; i++) {
       var radius = Math.sqrt(canvasWidth*canvasHeight/20/numberProjects)+15;
       var project = Bodies.circle(canvasWidth/2, 0-radius*i, radius, {
@@ -58,4 +158,5 @@ klimaChallenge.controller('ChallengeCtrl', function($scope, $http) {
 
    // run the engine
    Engine.run(engine);
+   */
 });
