34533132d52f0360e32359b1071f8fba3b116501
Adrian Jagusch <adrian.jagusch@we-mail.de>
2015-08-22 15:43:49 +0200

    Formel zur Berechnung des Kugelradius verbessert und BallArea-Höhe variabel gemacht

---
+++ bower_components/angular-flip/README.md
@@ -0,0 +1,38 @@
+#angular-flip directive
+***
+[AngularJS](http://angularjs.org/) directive that allows to achieve 3D card flip
+effect.
+
+[Demo](http://plnkr.co/edit/wBXlVC9DNj9uDJmqgnKK?p=preview)
+
+##Quick start
+***
+
+Load `angular-flip.css` and `angular-flip.js` into your html:
+```html
+<link rel="stylesheet" href="/path/to/angular-flip.css" />
+<script src="/path/to/angular-flip.js"></script>
+```
+
+Inject `angular-flip` into your application module:
+```javascript
+var app = angular.module('MyApp', ['angular-flip']);
+```
+
+Have fun:
+```html
+<!-- Vertical flip -->
+<flip flip-toggle>
+    <flip-front>Visible content goes there</flip-front>
+    <flip-back>The other side</flip-back>
+</flip>
+<!-- Horizontal flip -->
+<flip flip-toggle horizontal>
+    <flip-front>Visible content goes there</flip-front>
+    <flip-back>The other side</flip-back>
+</flip>
+```
+
+##License
+***
+`angular-flip` uses the [MIT](http://opensource.org/licenses/MIT) license
--- controllers/challenge.js
+++ controllers/challenge.js
@@ -1,5 +1,8 @@
 klimaChallenge.controller('ChallengeCtrl', function($scope, $http, $timeout) {
 
+   var numberProjects = 50;
+   var ballRadius = Math.pow(window.innerWidth, 1/2) * Math.pow(1/numberProjects, 1/10) + 30;
+
    Physics(function (world) {
        // bounds of the window
        var viewportBounds = Physics.aabb(0, 0, window.innerWidth, window.innerHeight),
@@ -37,15 +40,14 @@ klimaChallenge.controller('ChallengeCtrl', function($scope, $http, $timeout) {
        }, 500);
 
        // create some bodies
-      var numberProjects = 25;
+
       for (var i = 0; i < numberProjects; i++) {
-         var radius = Math.sqrt(renderer.width*20/numberProjects)+21;
-         radius = radius * (Math.random() * 1.3 + 0.7);
+         var thisRadius = ballRadius * (Math.random() * 1.05 + 0.95);
          var project = Physics.body('circle', {
            x: renderer.width * 0.5,
            y: -i*1000,
            vx: 0.3,
-           radius: radius,
+           radius: thisRadius,
            styles: {
                fillStyle: '#8bc34a'
            }
@@ -68,4 +70,14 @@ klimaChallenge.controller('ChallengeCtrl', function($scope, $http, $timeout) {
            world.step( time );
        });
    });
+
+   $scope.ballAreaHeight = function() {
+
+      var areaHeight = Math.pow(ballRadius*2, 2) * numberProjects/window.innerWidth * 2.1 + 220;
+
+      // Ball area shall be at least 400px high
+      if (areaHeight < 400)
+         return 400;
+      return areaHeight;
+   }
 });
