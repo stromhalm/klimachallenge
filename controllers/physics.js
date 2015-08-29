1725c0fbef9b23176a3c241ca7f83a5f345c2d66
Adrian Jagusch <adrian.jagusch@we-mail.de>
2015-08-29 23:38:53 +0200

    Kugeln bleiben nicht mehr in der Luft hängen

--- controllers/physics.js
+++ controllers/physics.js
@@ -4,39 +4,39 @@ klimaChallenge.controller('PhysicsCtrl', function($scope, $timeout) {
    var ballRadius = Math.pow(window.innerWidth, 1/2) * Math.pow(1/numberProjects, 1/10) + 30;
 
    Physics(function (world) {
-       // bounds of the window
-       var viewportBounds = Physics.aabb(0, 0, window.innerWidth, window.innerHeight),
-         edgeBounce,
-         renderer;
+      // world bounds to the window
+      var viewportBounds = Physics.aabb(0, 0, window.innerWidth, window.innerHeight),
+      edgeBounce,
+      renderer;
 
-       // create a renderer
-       renderer = Physics.renderer('canvas', {
-           el: 'physics-world'
-       });
+      // create a renderer
+      renderer = Physics.renderer('canvas', {
+         el: 'physics-world'
+      });
 
-       // add the renderer
-       world.add(renderer);
-       // render on each step
-       world.on('step', function () {
-           world.render();
-       });
+      // add the renderer
+      world.add(renderer);
+      // render on each step
+      world.on('step', function () {
+        world.render();
+      });
 
-       // constrain objects to these bounds
-       edgeBounce = Physics.behavior('edge-collision-detection', {
-           aabb: viewportBounds,
-           restitution: 0.4
-       });
+      // constrain objects to these bounds
+      edgeBounce = Physics.behavior('edge-collision-detection', {
+         aabb: viewportBounds,
+         restitution: 0.4
+      });
 
-       // resize canvas on canvas resize
-       window.addEventListener('resize', function () {
-           viewportBounds = Physics.aabb(0, 0, renderer.width, renderer.height);
-           edgeBounce.setAABB(viewportBounds);
-       }, true);
+      // resize canvas on canvas resize
+      window.addEventListener('resize', function () {
+         viewportBounds = Physics.aabb(0, 0, renderer.width, renderer.height);
+         edgeBounce.setAABB(viewportBounds);
+      }, true);
 
-       // At init the canvas won't load the correct window height, so refresh aufter 0.5s
-       $timeout(function() {
-          window.dispatchEvent(new Event('resize'));
-       }, 500);
+      // At init the canvas won't load the correct window height, so refresh aufter 0.5s
+      $timeout(function() {
+         window.dispatchEvent(new Event('resize'));
+      }, 500);
 
        // create some bodies
       for (var i = 0; i < numberProjects; i++) {
@@ -53,20 +53,28 @@ klimaChallenge.controller('PhysicsCtrl', function($scope, $timeout) {
          world.add(project);
       }
 
-       // add behaviors to the world
-       world.add([
-          Physics.behavior('interactive', { el: renderer.container }),
-          Physics.behavior('constant-acceleration', {acc: { x : 0, y: 0.0005 }}),
-          Physics.behavior('body-impulse-response'),
-          Physics.behavior('body-collision-detection'),
-          Physics.behavior('sweep-prune'),
-          edgeBounce
-       ]);
+      // add behaviors to the world
+      world.add([
+         Physics.behavior('interactive', { el: renderer.container }),
+         Physics.behavior('constant-acceleration', {acc: { x : 0, y: 0.0005 }}),
+         Physics.behavior('body-impulse-response'),
+         Physics.behavior('body-collision-detection'),
+         Physics.behavior('sweep-prune'),
+         edgeBounce
+      ]);
 
-       // subscribe to ticker to advance the simulation
-       Physics.util.ticker.on(function( time ) {
-           world.step( time );
-       });
+      // Tick all bodies on page change to awake them
+      $scope.$on('$locationChangeStart', function(event) {
+         var bodies = world.getBodies();
+         angular.forEach(bodies, function(body) {
+            body.applyForce({x: 0, y: .1});
+         });
+      });
+
+      // subscribe to ticker to advance the simulation
+      Physics.util.ticker.on(function( time ) {
+        world.step( time );
+      });
    });
 
    $scope.getBallAreaHeight = function() {
