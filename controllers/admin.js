0435cdec17e8d12e94eaa40bd0f157a7face32ab
Adrian <adrian.jagusch@uni-oldenburg.de>
2015-11-18 00:07:20 +0100

    Funktionsfähiges Login-System

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,12 +1,26 @@
 klimaChallenge.controller('adminCtrl', function($scope, Auth, projects) {
 
    $scope.auth = Auth;
+   $scope.email = "";
+   $scope.password = "";
 
-    // any time auth status updates, add the user data to scope
-    $scope.auth.$onAuth(function(authData) {
+   // any time auth status updates, add the user data to scope
+   $scope.auth.$onAuth(function(authData) {
       $scope.authData = authData;
-    });
+   });
 
+   $scope.login = function(email, password) {
+
+      Auth.$authWithPassword({
+         email: email,
+         password: password
+      }).then(function(authData) {
+
+      }).catch(function(error) {
+         $scope.loginError = true;
+         console.error("Authentication failed:", error);
+      });
+   }
    /*
 
    projects.$add({
