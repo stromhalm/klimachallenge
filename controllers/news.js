244304564e22701e0e811c0a424a302f0d97fe4e
Adrian <adrian.jagusch@uni-oldenburg.de>
2016-01-15 23:45:46 +0100

    Auswahl der Bilder verbessert

--- controllers/news.js
+++ controllers/news.js
@@ -2,25 +2,29 @@ klimaChallenge.controller('newsCtrl', function($scope, $http, $sce) {
 
    $scope.facebookImages = Array();
 
-   $http.get('https://graph.facebook.com/v2.4/klimachallenge/photos/uploaded?fields=link,height,name,images&limit=5&access_token=846767055411205|UKF39DbxTvvEeA9BuKkWsJgiuLE').
+   $http.get('https://graph.facebook.com/v2.4/klimachallenge/photos/uploaded?fields=link,width,name,images&limit=6&access_token=846767055411205|UKF39DbxTvvEeA9BuKkWsJgiuLE').
    success(function(data, status, headers, config) {
 
       angular.forEach(data.data, function(image, key) {
-         if (image.name) {
 
-            // Shorten image text if longer than 160 chars
-            if (image.name.length > 160) {
-               var newText = image.name.substring(0,140);
-               var i = 140;
-               while (image.name.charAt(i) != " ") {
-                  newText = newText + image.name.charAt(i);
-                  i++;
+         // Only take images when they are big enough
+         if (image.width >= 720) {
+            if (image.name) {
+
+               // Shorten image text if longer than 160 chars
+               if (image.name.length > 160) {
+                  var newText = image.name.substring(0,140);
+                  var i = 140;
+                  while (image.name.charAt(i) != " ") {
+                     newText = newText + image.name.charAt(i);
+                     i++;
+                  }
+                  image.name = newText;
+                  image.showReadMore = true;
                }
-               image.name = newText;
-               image.showReadMore = true;
             }
+            $scope.facebookImages.push(image);
          }
-         $scope.facebookImages.push(image);
       });
    });
 });
